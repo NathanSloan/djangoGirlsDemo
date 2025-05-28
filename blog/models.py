7e246357d6421a883ea576be990b8264bdab0621
Natha <nathansloan2003@gmail.com>
2025-05-28 16:53:50 -0400

    djangoGirls commit 1

---
+++ blog/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+# Create your models here.
+
+# class - defines an object
+# models.Model - states object is a Django Model, saves to database
+class Post(models.Model):
+    # models.ForeignKey - states attribute is a link to another model
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    # models.CharField - states attribute is text with a limited number of characters
+    title = models.CharField(max_length=200)
+    # models.TextField - states attribute is text of an undefined length
+    text = models.TextField()
+    # models.DateTimeField - states attribute is a field of date/time
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    # def - defines a method
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
